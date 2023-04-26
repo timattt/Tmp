783ae0b9b6d24f1c06f3dfd0c17ef1d036bd767a
Tim Trofimenko <timattt@gmail.com>
2023-04-26 16:01:06 +0300

    Update LazyString.cpp

--- Tasks/LazyString.cpp
+++ Tasks/LazyString.cpp
@@ -1,35 +1,31 @@
-//
-// Created by timat on 26.04.2023.
-//
-
 #include <cstring>
 #include <iostream>
 #include "LazyString.h"
 
-LazyString::LazyString(const char *cStr) {
+CLazyString::CLazyString(const char *cStr) {
     m_length = strlen(cStr);
     m_data = new char[m_length];
     memccpy(m_data, cStr, 1, m_length);
     m_counter = new int(1);
 }
 
-LazyString::LazyString(const LazyString &other) : m_data(other.m_data), m_length(other.m_length), m_counter(other.m_counter) {
+CLazyString::CLazyString(const CLazyString &other) : m_data(other.m_data), m_length(other.m_length), m_counter(other.m_counter) {
     (*m_counter)++;
 }
 
-LazyString::LazyString(const LazyString &&other) : LazyString(other) {
+CLazyString::CLazyString(const CLazyString &&other) : CLazyString(other) {
 }
 
-char &LazyString::operator[](int pos) {
+char &CLazyString::operator[](int pos) {
     EnsureUnique();
     return m_data[pos];
 }
 
-char LazyString::operator[](int pos) const {
+char CLazyString::operator[](int pos) const {
     return m_data[pos];
 }
 
-void LazyString::Print() const {
+void CLazyString::Print() const {
     std::cout << "Lazy string. counter=" << *m_counter << " [";
     for (int i = 0; i < m_length; i++) {
         std::cout << m_data[i];
@@ -37,7 +33,7 @@ void LazyString::Print() const {
     std::cout << "] id=" << (long long)(m_counter)%1000 << "\n";
 }
 
-LazyString::~LazyString() {
+CLazyString::~CLazyString() {
     (*m_counter)--;
     if (*m_counter == 0) {
         delete m_counter;
@@ -45,7 +41,7 @@ LazyString::~LazyString() {
     }
 }
 
-void LazyString::EnsureUnique() {
+void CLazyString::EnsureUnique() {
     if (*m_counter > 1) {
         (*m_counter)--;
         m_counter = new int(1);
@@ -55,7 +51,7 @@ void LazyString::EnsureUnique() {
     }
 }
 
-void LazyString::operator+=(const LazyString &other) {
+void CLazyString::operator+=(const CLazyString &other) {
     EnsureUnique();
     char * old = m_data;
     int oldLen = m_length;
