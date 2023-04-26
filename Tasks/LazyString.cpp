5e3663c38cc33129eeba5120c4329ac5b20e64f6
Tim Trofimenko <timattt@gmail.com>
2023-04-26 16:04:44 +0300

    Update LazyString.cpp

--- Tasks/LazyString.cpp
+++ Tasks/LazyString.cpp
@@ -2,26 +2,31 @@
 #include <iostream>
 #include "LazyString.h"
 
-CLazyString::CLazyString(const char *cStr) {
+CLazyString::CLazyString(const char *cStr)
+{
     m_length = strlen(cStr);
     m_data = new char[m_length];
     memccpy(m_data, cStr, 1, m_length);
     m_counter = new int(1);
 }
 
-CLazyString::CLazyString(const CLazyString &other) : m_data(other.m_data), m_length(other.m_length), m_counter(other.m_counter) {
+CLazyString::CLazyString(const CLazyString &other) : m_data(other.m_data), m_length(other.m_length), m_counter(other.m_counter)
+{
     (*m_counter)++;
 }
 
-CLazyString::CLazyString(const CLazyString &&other) : CLazyString(other) {
+CLazyString::CLazyString(const CLazyString &&other) : CLazyString(other)
+{
 }
 
-char &CLazyString::operator[](int pos) {
+char &CLazyString::operator[](int pos)
+{
     EnsureUnique();
     return m_data[pos];
 }
 
-char CLazyString::operator[](int pos) const {
+char CLazyString::operator[](int pos) const
+{
     return m_data[pos];
 }
 
@@ -30,10 +35,11 @@ void CLazyString::Print() const {
     for (int i = 0; i < m_length; i++) {
         std::cout << m_data[i];
     }
-    std::cout << "] id=" << (long long)(m_counter)%1000 << "\n";
+    std::cout << "] id=" << (long long)(m_counter) % 1000 << "\n";
 }
 
-CLazyString::~CLazyString() {
+CLazyString::~CLazyString()
+{
     (*m_counter)--;
     if (*m_counter == 0) {
         delete m_counter;
@@ -41,7 +47,8 @@ CLazyString::~CLazyString() {
     }
 }
 
-void CLazyString::EnsureUnique() {
+void CLazyString::EnsureUnique()
+{
     if (*m_counter > 1) {
         (*m_counter)--;
         m_counter = new int(1);
@@ -51,7 +58,8 @@ void CLazyString::EnsureUnique() {
     }
 }
 
-void CLazyString::operator+=(const CLazyString &other) {
+void CLazyString::operator+=(const CLazyString &other)
+{
     EnsureUnique();
     char * old = m_data;
     int oldLen = m_length;
